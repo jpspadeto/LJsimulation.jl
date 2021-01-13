14f198a70378b0ec60468f9768bd58508df671c5
João Paulo Menezes Spadeto <jpspadeto@gmail.com>
2021-01-13 11:59:21 -0300

    Create force_energy.jl

---
+++ src/force_energy.jl
@@ -0,0 +1,18 @@
+#Computes the force and energy of every particles of the system 
+
+function total_force!(p,f,Data)
+    ut = 0.
+    for x in 1:length(f)
+      f[x] .= 0.
+    end
+      for i in 1:Data.N-1, j in i+1:Data.N
+        dist = dmin(p[i],p[j],Data.side)
+          if dist <= Data.cutoff
+          upair, fpair = force_pair(p[i],p[j],dist,Data)
+          ut += upair
+            f[i] .= f[i] .+ fpair
+            f[j] .= f[j] .- fpair
+          end
+      end
+    return ut, f
+  end
